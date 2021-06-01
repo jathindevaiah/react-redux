8435c6ce639b5ec98ef1fbdefc7a1b27d2fea78c
Jathin Devaiah <devaiah.jathin@gmail.com>
2021-06-01 18:20:25 -0400

    Adding redux toolkit and use createSlice for store

--- src/store/index.js
+++ src/store/index.js
@@ -1,32 +1,25 @@
-import { createStore } from 'redux';
+import { createSlice, configureStore } from '@reduxjs/toolkit';
 
 const initialState = { counter: 0, showCounter: true };
 
-const counterReducer = (state = initialState, action) => {
-  const { type, payload } = action;
+const counterSlice = createSlice({
+  name: 'counter',
+  initialState,
+  reducers: {
+    increment(state, action) {
+      state.counter = state.counter + action.payload;
+    },
+    decrement(state, action) {
+      state.counter = state.counter - action.payload;
+    },
+    toggle(state) {
+      state.showCounter = !state.showCounter;
+    },
+  },
+});
 
-  if (type === 'increment') {
-    return {
-      counter: state.counter + payload,
-      showCounter: state.showCounter,
-    };
-  }
-  if (type === 'decrement') {
-    return {
-      counter: state.counter - payload,
-      showCounter: state.showCounter,
-    };
-  }
-  if (type === 'toggle') {
-    return {
-      showCounter: !state.showCounter,
-      counter: state.counter,
-    };
-  }
-
-  return state;
-};
-
-const store = createStore(counterReducer);
+const store = configureStore({
+  reducer: counterSlice.reducer,
+});
 
 export default store;
